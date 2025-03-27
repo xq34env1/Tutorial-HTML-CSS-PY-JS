139cef0af248211ab003f8d59b372a778add47c3
Labrat <89636989+xq34env1@users.noreply.github.com>
2025-03-27 10:15:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,51 @@ from flask import Flask, render_template, request, redirect
 import os
 from os import path
 
-global whichfilename
+global whichfilename;
+whichfilename = "LoginAccounts.doc"
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/info", methods=["POST"])
+def GetInfo():
+    global username
+    global userpasswd
+    username = request.form.get('txtusername')
+    userpasswd = request.form.get('txtpassword')
+    if(username == "" or userpasswd == ""):
+        return render_template("index.html")
+    else:
+        CreateCheckFile()
+        return render_template("index2.html", username = username, password = userpasswd)
+
+def CreateCheckFile():
+    fileDir = os.path.dirname(os.path.realpath("__file__"))
+    fileexist = bool(path.exists(whichfilename))
+    if(fileexist == False):
+        status = "new"
+    else:
+        status = "edit"
+
+    WriteToFile(status)
+
+def WriteToFile(whichstatus):
+    if(whichstatus == "new"):
+        logacctfile = open(whichfilename, "x")
+        logacctfile.close()
+        logacctfile = open(whichfilename, "w")
+    else:
+        logacctfile = open(whichfilename, "a")
+
+    logacctfile.write(username + " " + userpasswd)
+    ReadFromFile()
+
+def ReadFromFile():
+    logacctfile = open(whichfilename, "r")
+    username_passwords = logacctfile.readlines()
+    print(username_passwords)
+
+if __name__ == "__main__":
+    app.run()
